a08b411b92a28b4d0cd3d726cbc7a5cb7621c66d
DphnAgl <nanastifler@gmail.com>
2022-10-21 18:55:06 +0300

    initial commit

---
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
+const date = new Date();
+const currentYear = date.getFullYear();
+
+const Expenses = (props) => {
+	const [
+		selectedYear,
+		setSelectedYear
+	] = useState(currentYear);
+
+	const filterChangeHandler = (selectedFilterYear) => {
+		setSelectedYear(selectedFilterYear);
+	};
+
+	const filteredExpenses = props.expenses.filter((expense) => {
+		return expense.date.getFullYear().toString() === selectedYear;
+	});
+
+	return (
+		<div>
+			<Card className="expenses">
+				<ExpensesFilter selectedYear={selectedYear} onChangeFilter={filterChangeHandler} />
+				<ExpensesChart expenses={filteredExpenses} />
+				<ExpensesList expenses={filteredExpenses} />
+			</Card>
+		</div>
+	);
+};
+
+export default Expenses;
